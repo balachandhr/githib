feefefb0be9c05c8c8bade5a6b62bd2f24a179d6
Balachandhr Vasantha Sadhasivam <40868170+balachandhr@users.noreply.github.com>
2018-09-05 21:39:16 -0400

    change

---
+++ demo-1/src/main/java/com/example/demo1/Employee.java
@@ -0,0 +1,21 @@
+package com.example.demo1;
+
+public class Employee {
+	
+	String name;
+	Integer age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String names) {
+		this.name = names;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age= age;
+	}
+	
+
+}
